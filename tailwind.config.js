d6f18acf54d38f0875840345b7e66364d0111dfe
Nerlih Max <nerlihmax@yandex.ru>
2022-02-14 20:18:54 +0700

    засетапил prettier поставил либы всякие мелкие приколы с конфигами

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 'blue-powder': '#A1D2CE',
+        // 'blue-middle': '#78CAD2',
+        // 'blue-cadet': '#62A8AC',
+        // 'blue-munsell': '#62A8AC',
+        // 'blue-munsell-darker': '#4B8695',
+        // 'teal-steel': '#50858B',
+      },
+    },
   },
   plugins: [],
 };
